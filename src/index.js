3396bd05852856e420458960168c4a60ccc62a5f
Wreulicke <masaya2dies@gmail.com>
2016-11-03 17:30:55 +0900

    add react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("axxxaaasssssssaaaasx")
+import React from "react";
+import ReactDOM from "react-dom";
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+const Application=()=> <div>test</div>
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Application/>, document.getElementById("container"));
+});
