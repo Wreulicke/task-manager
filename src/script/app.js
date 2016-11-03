fba55c6ccf5ab2818d732ee76df30834c1bd3a85
Wreulicke <masaya2dies@gmail.com>
2016-11-03 19:50:58 +0900

    implemented simple todo app

--- src/script/app.js
+++ src/script/app.js
@@ -2,14 +2,55 @@ import React from "react";
 import AppBar from "material-ui/AppBar";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
+import Done from "material-ui/svg-icons/action/done";
 
-const App = () => (
-  <MuiThemeProvider muiTheme={ getMuiTheme() }>
-    <div>
-      <AppBar
-              iconElementLeft={ <span/> }
-              title="task manager" />
-    </div>
-  </MuiThemeProvider>
-);
+import { ListItem } from "material-ui/List";
+import TextField from "material-ui/TextField";
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      tasks: []
+    }
+  }
+  done(index) {
+    const tasks = this.state.tasks.filter((_, i) => index !== i)
+    this.setState({
+      tasks
+    })
+  }
+  add(task) {
+    const tasks = this.state.tasks.concat(task)
+    this.setState({
+      tasks
+    })
+  }
+  onEnter(e) {
+    if (e.ctrlKey && e.key === "Enter") {
+      this.add(e.target.value)
+      e.target.value = ""
+    }
+  }
+  render() {
+    const tasks = this.state.tasks.map((task, i) => {
+      return <ListItem
+                       leftIcon={ <Done onClick={ () => this.done(i) } /> }
+                       key={ i }
+                       primaryText={ task } />
+    })
+    return <MuiThemeProvider muiTheme={ getMuiTheme() }>
+             <div>
+               <AppBar
+                       iconElementLeft={ <span/> }
+                       title="task manager" />
+               <TextField
+                          hintText="test"
+                          onKeyDown={ ::this.onEnter } />
+               <div>
+                 { tasks }
+               </div>
+             </div>
+           </MuiThemeProvider>
+  }
+}
 export default App;
